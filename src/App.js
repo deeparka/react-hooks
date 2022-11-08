655120a07d92df11377eeb78fddda4047cdbfa78
deeparka <arkadeep180@gmail.com>
2022-11-08 16:43:09 +0530

    useMemo done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import UseEffect from "./hooks/UseEffect";
+import UseMemo from "./hooks/UseMemo";
 import UseRef from "./hooks/UseRef";
 
 function App() {
     return (
         <div className="App">
             {/* <UseEffect /> */}
-            <UseRef />
+            {/* <UseRef /> */}
+            <UseMemo />
         </div>
     );
 }
