cc2c494da7a339e78137b62b938a100112a3d08b
deeparka <arkadeep180@gmail.com>
2022-11-09 11:56:30 +0530

    useRef done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import UseEffect from "./hooks/UseEffect";
 import UseRef from "./hooks/UseRef";
 
 function App() {
     return (
         <div className="App">
-            {/* <UseEffect /> */}
             <UseRef />
         </div>
     );
