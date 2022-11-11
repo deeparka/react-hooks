b90438a55c05d0a1d52c47ad099c6dfac5149ea5
deeparka <arkadeep180@gmail.com>
2022-11-11 12:35:32 +0530

    useLocalStorage custom hook done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
-import UseReducer from "./hooks/UseReducer";
+import useLocalStorage from "./custom hook/useLocalStorage";
 
 function App() {
+    const [name, setName] = useLocalStorage("name", "");
     return (
         <div>
-            <UseReducer />
+            <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
         </div>
     );
 }
